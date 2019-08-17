08223fcf62d7548493fc7b60f2618ef6335d9333
Johan Bjäreholt <johan@bjareho.lt>
2019-08-17 21:43:19 +0200

    main: Add more search paths for asset dir

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,6 @@ pub mod logging;
 pub mod config;
 
 fn main() {
-    use std::path::PathBuf;
     use std::sync::Mutex;
 
     logging::setup_logger().expect("Failed to setup logging");
@@ -48,10 +47,65 @@ fn main() {
     let db_path = dirs::db_path().to_str().unwrap().to_string();
     info!("Using DB at path {:?}", db_path);
 
+    let asset_path = get_asset_path();
+    info!("Using aw-webui assets at path {:?}", asset_path);
+
     let server_state = endpoints::ServerState {
         datastore: Mutex::new(datastore::Datastore::new(db_path)),
-        asset_path: PathBuf::from("aw-webui").join("dist"),
+        asset_path: asset_path,
     };
 
     endpoints::build_rocket(server_state, &config).launch();
+}
+
+use std::path::PathBuf;
+
+fn get_asset_path () -> PathBuf {
+    use std::env::current_exe;
+
+    // TODO: Add cmdline arg which can override asset path?
+
+    // Search order for asset path is:
+    // 1. ./aw-webui/dist
+    // 2. $current_exe_dir/aw_server_rust/static
+    // 3. ./aw_server_rust/static
+    // 4. (fallback) ./aw-webui/dist
+
+    // cargo_dev_path
+    // (for running with cargo run)
+    let cargo_dev_path = PathBuf::from("./aw-webui/dist/");
+    if cargo_dev_path.as_path().exists() {
+        return cargo_dev_path;
+    }
+
+    // current_exe_path
+    // (for self-contained deployed binaries)
+    match current_exe() {
+        Ok(mut current_exe_path) => {
+            current_exe_path.pop(); // remove name of executable
+            current_exe_path.push("aw_server_rust/static/");
+            if current_exe_path.as_path().exists() {
+                return current_exe_path;
+            }
+        },
+        Err(_) => (),
+    };
+
+    // cwd_path
+    // (for linux opt installs)
+    let cwd_path = PathBuf::from("./aw_server_rust/static/");
+    if cwd_path.as_path().exists() {
+        return cwd_path;
+    }
+
+    // usr_path
+    // (for linux usr installs)
+    let mut usr_path = appdirs::site_data_dir(Some("activitywatch"), None).unwrap();
+    usr_path.push("aw_server_rust/static");
+    if usr_path.as_path().exists() {
+        return cwd_path;
+    }
+
+    warn!("Unable to find an aw-webui asset path which exists, falling back to ./aw-webui/dist");
+    return cargo_dev_path;
 }
